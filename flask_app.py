b770ad9fdac893d51649669bb3ed3d4585c566af
pratiksha05 <pratikshamore4215@gmail.com>
2018-02-02 12:13:42 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -51,23 +51,24 @@ predicted = classifier.predict(data[n_samples // 2:])
 def predictions():
     data = request.get_json()
     predicted = classifier.predict([data])
-    """
+    '''
     print("Classification report for classifier %s:\n%s\n"
           % (classifier, metrics.classification_report(expected, predicted)))
     print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
 
     images_and_predictions = list(zip(digits.images[n_samples // 2:], predicted))
-    for index, (image, prediction) in enumerate(images_and_predictions[:4]):
-        plt.subplot(2, 4, index + 5)
-        plt.axis('off')
-        plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    '''
+    #for index, (image, prediction) in enumerate(images_and_predictions[:4]):
+    #    plt.subplot(2, 4, index + 5)
+    #    plt.axis('off')
+    #    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
       
-        plt.title('Predictions: %i' % predicted)
+    #    plt.title('Predictions: %i' % predicted)
      
-        """  
+          
     return("predicted :{}".format(predicted))
     
-    plt.show()
+    #plt.show()
 
 
 if __name__ == "__main__":
